9ea86f2efd8a7d28f2340a3d7e819de6e5145be4
bobo <pauleric789456@gmail.com>
2021-11-28 17:52:06 +0800

    hw commit

--- src/main/java/fcu/sep/fcushop/FcushopApplication.java
+++ src/main/java/fcu/sep/fcushop/FcushopApplication.java
@@ -3,7 +3,6 @@ package fcu.sep.fcushop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/** this is */
 @SpringBootApplication
 public class FcushopApplication {
 
--- src/main/java/fcu/sep/fcushop/service/ProductService.java
+++ src/main/java/fcu/sep/fcushop/service/ProductService.java
@@ -2,37 +2,40 @@ package fcu.sep.fcushop.service;
 
 import fcu.sep.fcushop.database.Sql2oDbHandler;
 import fcu.sep.fcushop.model.Product;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.sql2o.Connection;
 
-
-/** CA. */
+import java.util.List;
 
 @Service
 public class ProductService {
 
   @Autowired
-    private Sql2oDbHandler sql2oDbHandler;
+  private Sql2oDbHandler sql2oDbHandler;
 
   public ProductService() {
 
   }
-  /** CA. */
 
   public List<Product> getProducts() {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
       String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
-            + " from PRODUCT";
+          + " from PRODUCT";
+
       return connection.createQuery(query).executeAndFetch(Product.class);
     }
   }
-  public List<Product> searchProducts(String name) {
+
+  public List<Product> getProducts(String keyword) {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
       String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
-      + " from PRODUCT  WHERE NAME = '"+name+"';";
-      return connection.createQuery(query).executeAndFetch(Product.class);
+          + " from PRODUCT where name like :keyword";
+
+      return connection.createQuery(query)
+          .addParameter("keyword", "%"+keyword+"%")
+          .executeAndFetch(Product.class);
     }
   }
+
 }
